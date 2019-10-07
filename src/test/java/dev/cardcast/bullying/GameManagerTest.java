8bb7f22353f87b4fb2e12859326226516666146c
Aaron <aaron.goes@hotmail.com>
2019-10-07 14:36:41 +0200

    Fix tests

--- src/test/java/dev/cardcast/bullying/GameManagerTest.java
+++ src/test/java/dev/cardcast/bullying/GameManagerTest.java
@@ -1,5 +1,6 @@
 package dev.cardcast.bullying;
 
+import dev.cardcast.bullying.entities.Game;
 import dev.cardcast.bullying.entities.Lobby;
 import dev.cardcast.bullying.entities.Player;
 import org.junit.jupiter.api.Assertions;
@@ -17,33 +18,45 @@ public class GameManagerTest {
     }
 
     @Test
-    public void testFindLobbyByCode(){
-        Lobby lobby2 = gameManager.findLobbyByCode(lobby.getCode());
-        Assertions.assertEquals(lobby, lobby2);
+    public void testTryJoinLobby(){
+        Lobby lobby2 = gameManager.createLobby(true, 2);
+        gameManager.tryJoinLobby(player, lobby2.getCode());
+        int playerCount = lobby.getQueued().size();
+        gameManager.addPlayer(lobby2, player);
+        Assertions.assertEquals(lobby2.getQueued().size(), playerCount + 1);
     }
 
     @Test
     public void testAddPlayer(){
         Lobby lobby2 = gameManager.createLobby(true, 2);
-        int playerCount = lobby2.queued.size();
+        int playerCount = lobby2.getQueued().size();
         gameManager.addPlayer(lobby2, player);
-        Assertions.assertEquals(lobby2.queued.size(), playerCount + 1);
+        Assertions.assertEquals(lobby2.getQueued().size(), playerCount + 1);
     }
 
     @Test
     public void testRemovePlayer(){
         Lobby lobby2 = gameManager.createLobby(true, 2);
         gameManager.addPlayer(lobby2, player);
-        int playerCount = lobby2.queued.size();
+        int playerCount = lobby2.getQueued().size();
         gameManager.removePlayer(lobby2, player);
-        Assertions.assertEquals(lobby2.queued.size(), playerCount - 1);
+        Assertions.assertEquals(lobby2.getQueued().size(), playerCount - 1);
+    }
+
+    @Test
+    public void testPlayerReadyUp(){
+        Lobby lobby2 = gameManager.createLobby(true, 2);
+        gameManager.addPlayer(lobby2, player);
+        boolean isReady = gameManager.playerReadyUp(lobby2, player);
+        Assertions.assertTrue(isReady);
     }
 
     @Test
     public void testStartGame(){
-//        Lobby lobby2 = gameManager.createLobby(true, 2);
-//        gameManager.addPlayer(lobby2, player);
-//        gameManager.startGame(lobby);
-//        Assertions.assertEquals(lobby2.queued.size(), playerCount - 1);
+        Lobby lobby2 = gameManager.createLobby(true, 2);
+        gameManager.addPlayer(lobby2, player);
+        gameManager.playerReadyUp(lobby2, player);
+        Game game = gameManager.startGame(lobby);
+        Assertions.assertNotNull(game);
     }
 }
